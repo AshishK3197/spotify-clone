c967b3981373faf1ba361cee383ff9de0f06488f
AshishK3197 <ashishk3197@gmail.com>
2021-01-01 00:44:48 +0530

    Configired the login component UI and functionality to hit authorization endpoint and obtain a access token from URl generated after redirect back to localhost:3000

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import Login from "./Components/Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+	useEffect(() => {
+		const hash = getTokenFromUrl();
+		console.log(hash);
+	}, []);
+
 	return (
 		<div className="App">
-			<h1>Spotify Clone 🚀</h1>
+			<Login />
 		</div>
 	);
 }
